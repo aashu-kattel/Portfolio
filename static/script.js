f8120ba85203753dfff6d0b6fd154e4dca975828
aashu-kattel <kattelaashu1@gmail.com>
2024-03-25 15:22:23 +0545

    prevent viewsource

--- static/script.js
+++ static/script.js
@@ -24,4 +24,14 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     }
   });
+
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.key === 'I') {
+        e.preventDefault();
+    }
+});
   
