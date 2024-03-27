438ff93a0c0d08efcb5876bae62a9c6fddc521cd
aashu-kattel <kattelaashu1@gmail.com>
2024-03-27 12:16:08 +0545

    progress bar added

--- static/script.js
+++ static/script.js
@@ -1,3 +1,13 @@
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;  
+    var scrolled = (winScroll / height) * 100;
+  
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
   
